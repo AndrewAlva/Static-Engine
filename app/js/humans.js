e6673ff51746fd22d751ff3f7a45e6ae12315120
Andrew Alvarado <andrew@activetheory.net>
2021-01-11 00:33:18 -0600

    Imported brunch and grid system from Rizo project, pending to import the rest of lighthouse optimizations.

---
+++ app/js/humans.js
@@ -0,0 +1,8 @@
+console.log("TEAM:");
+console.log("Frontend Development: Andrew Alvarado https://andrew.studio/");
+
+console.log("");
+console.log("");
+
+console.log("THANKS:");
+console.log("https://www.rizo-olvera.com/humans.txt");
---
+++ brunch-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	files: {
+		javascripts: {
+			joinTo: 'js/app.js',
+			order: {
+				before: 'app/js/initialize.js',
+				after: 'app/js/humans.js'
+			}
+		},
+		stylesheets: {joinTo: 'css/app.css'}
+	},
+
+	npm: {
+		enabled: false
+	},
+
+	modules: {
+		wrapper: false,
+		definition: false
+	}
+}
